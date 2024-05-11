0d3f892ab0a0b9f26d72d1a0147dc739e994fb0b
yamaguchi <yamaguchi@sutech.co.jp>
2024-05-11 16:52:25 +0900

    remove strict

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App.tsx'
@@ -14,28 +13,26 @@ import ResultPage from './pages/result/[id].tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="designer">
-          <Route path="gallery" element={<DesignerGalleryPage />} />
-          <Route path="new" element={<DesignerNewPage />} />
-          <Route path="edit">
-            <Route path=":layoutId" element={<DesignerEditPage />} />
-          </Route>
+  <BrowserRouter>
+    <Routes>
+      <Route path="designer">
+        <Route path="gallery" element={<DesignerGalleryPage />} />
+        <Route path="new" element={<DesignerNewPage />} />
+        <Route path="edit">
+          <Route path=":layoutId" element={<DesignerEditPage />} />
         </Route>
-        <Route path="import">
-          <Route path="gallery" element={<ImportGalleryPage />} />
-          <Route path="select" element={<ImportSelectLayoutsPage />} />
-          <Route path=":layoutId" element={<ImportPage />} />
-        </Route>
-        <Route path="result">
-          <Route path=":id" element={<ResultPage />} />
-        </Route>
-        <Route path="dictionary" element={<DictionaryPage />} />
-        <Route path="logo" element={<LogoPage />} />
-        <Route path="" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+      </Route>
+      <Route path="import">
+        <Route path="gallery" element={<ImportGalleryPage />} />
+        <Route path="select" element={<ImportSelectLayoutsPage />} />
+        <Route path=":layoutId" element={<ImportPage />} />
+      </Route>
+      <Route path="result">
+        <Route path=":id" element={<ResultPage />} />
+      </Route>
+      <Route path="dictionary" element={<DictionaryPage />} />
+      <Route path="logo" element={<LogoPage />} />
+      <Route path="" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 )
